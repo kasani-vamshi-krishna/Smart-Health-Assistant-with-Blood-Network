be3db5a4c9ef8517cdb7fcdca4160122e78e003b
kartheek kethavath <kartheekkethavath@kartheeks-MacBook-Air.local>
2026-03-15 21:29:28 +0530

    version-2 done v1

--- Multiple_Disease_Web_app/backend.py
+++ Multiple_Disease_Web_app/backend.py
@@ -550,10 +550,3 @@ if __name__ == '__main__':
         app.run(debug=True)
     else:
         print("FATAL: Could not start the application because one or more machine learning models failed to load.")
-
-
-         # new line a
-
-         # fixed this backend.py ready for prod. 
-
-         # this is the third fix 
